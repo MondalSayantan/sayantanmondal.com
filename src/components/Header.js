cfc19a62073d8c4e210cfe78c792a388cf2f9c71
Sayantan <sayantan.dps@gmail.com>
2022-12-13 18:44:07 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import * as styles from "./Header.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+import wave from "../assets/waving-hand.gif";
+
+const Header = () => {
+  return (
+    <header className={`${styles.bg} lg:h-screen w-full`}>
+      <div className="w-full max-w-7xl mx-auto flex flex-col lg:flex-row lg:h-screen md:h-3/4 h-full my-auto pt-20 justify-around items-center md:px-5 ">
+        <div className="flex flex-col w-3/4 lg:w-1/2">
+          <div className="flex items-center justify-center lg:justify-start lg:items-end">
+            <h1 className=" text-3xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
+              Hi
+            </h1>
+            <img src={wave} alt="gif" className="h-10 md:h-20 ml-1 lg:ml-5 " />
+          </div>
+          <div className="flex flex-col lg:flex-row items-center lg:items-end justify-center lg:justify-start">
+            <h1 className="text-3xl lg:py-4 font-bold tracking-tight leading-none text-gray-900 md:text-4xl mr-5 pb-5 md:pb-0">
+              I am
+            </h1>
+            <h1 className="md:text-6xl text-4xl text-center lg:py-4 font-extrabold bg-gradient-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent">
+              Sayantan Mondal
+            </h1>
+          </div>
+          <div>
+            {/* subtext */}
+            <p className="text-1xl text-justify font-semibold text-gray-900 mt-5">
+              With a background in web development and a love for all things
+              tech, I'm a full-stack developer and an aspiring software engineer
+              who's always on the lookout for the next big thing. I'm passionate
+              about using my skills to create intuitive, user-friendly websites
+              and applications that make people's lives easier.
+            </p>
+          </div>
+        </div>
+        <StaticImage
+          src="../assets/avatar.png"
+          alt="hero"
+          className="h-40 w-40 md:h-80 md:w-80 rounded-lg rotate-6"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import * as styles from "./Navbar.module.css";
+import {
+  AiOutlineMenu,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { CgClose } from "react-icons/cg";
+import { AnimatePresence, motion, useCycle } from "framer-motion";
+
+const itemVariants = {
+  closed: {
+    opacity: 0,
+  },
+  open: { opacity: 1 },
+};
+
+const sideVariants = {
+  closed: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: 1,
+    },
+  },
+};
+
+function Navbar() {
+  const [top, setTop] = useState(true);
+  const [open, cycleOpen] = useCycle(false, true);
+
+  const links = [
+    { name: "Home", link: "/", id: 1 },
+    { name: "Skills", link: "#skills", id: 2 },
+    { name: "Projects", link: "#projects", id: 3 },
+    { name: "Resume", link: "/resume", id: 4 },
+    { name: "Contact", link: "/contact", id: 6 },
+    { name: "Blog", link: "/blog", id: 5 },
+  ];
+
+  const socials = [
+    {
+      name: "Github",
+      link: "https://github.com/MondalSayantan",
+      id: "github",
+      icon: <AiFillGithub className="text-2xl ml-5" />,
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/mondalsayantan/",
+      id: "linkedin",
+      icon: <AiFillLinkedin className="text-2xl" />,
+    },
+    {
+      name: "Twitter",
+      link: "https://twitter.com/sayantannnnn",
+      id: "twitter",
+      icon: <AiOutlineTwitter className="text-2xl" />,
+    },
+  ];
+
+  // detect whether user has scrolled the page down by 10px
+  useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+    };
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, [top]);
+
+  return (
+    <header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top && "bg-white backdrop-blur-sm shadow-lg"
+      }`}
+    >
+      <AnimatePresence>
+        {open && (
+          <motion.aside
+            className="w-60 h-screen bg-white fixed top-0 left-0 z-40 shadow-lg"
+            initial={{ width: 0 }}
+            animate={{
+              width: 300,
+            }}
+            exit={{
+              width: 0,
+              transition: { delay: 0.7, duration: 0.3 },
+            }}
+          >
+            <motion.div
+              className="flex flex-col justify-start mt-20 items-center h-full"
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={sideVariants}
+            >
+              {links.map(({ name, to, id }) => (
+                <motion.a
+                  key={id}
+                  href={to}
+                  whileHover={{ scale: 1.1 }}
+                  variants={itemVariants}
+                  className="text-2xl font-bold text-gray-800 mb-10"
+                >
+                  {name}
+                </motion.a>
+              ))}
+            </motion.div>
+          </motion.aside>
+        )}
+      </AnimatePresence>
+      <div className="max-w-6xl md:max-w-7xl px-5 mx-auto sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          {/* Site branding */}
+          <div className="flex-shrink-0 mr-4">
+            {/* Logo */}
+            <div className="block" aria-label="logo">
+              <div className={styles.cubeWrapper}>
+                <div className={`${styles.cube} ${styles.streakCube}`}>
+                  <div
+                    className={`${styles.cubeFace} ${styles.cubeFront}`}
+                  ></div>
+                  <div
+                    className={`${styles.cubeFace} ${styles.cubeBack}`}
+                  ></div>
+                  <div
+                    className={`${styles.cubeFace} ${styles.cubeBottom}`}
+                  ></div>
+                  <div className={`${styles.cubeFace} ${styles.cubeTop}`}></div>
+                  <div
+                    className={`${styles.cubeFace} ${styles.cubeRight}`}
+                  ></div>
+                  <div
+                    className={`${styles.cubeFace} ${styles.cubeLeft}`}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Site navigation */}
+          <nav className="flex flex-grow ">
+            <ul className="lg:flex flex-grow justify-end flex-wrap items-center hidden">
+              {links.map(({ name, link, id }) => (
+                <li key={id} className="mx-7">
+                  <Link to={link}>{name}</Link>
+                </li>
+              ))}
+              {socials.map(({ name, link, id, icon }) => (
+                <li key={id} className="mx-2">
+                  <a href={link} target="_blank">
+                    {icon}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className="lg:hidden flex items-center flex-grow justify-end">
+              {/* <AiOutlineMenu className="text-2xl" onClick={cycleOpen} /> */}
+              {open ? (
+                <CgClose className="text-2xl" onClick={() => cycleOpen()} />
+              ) : (
+                <AiOutlineMenu
+                  className="text-2xl"
+                  onClick={() => cycleOpen()}
+                />
+              )}
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
