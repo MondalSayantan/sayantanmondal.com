87fef5dfe4e9abb0ca0708d3a8cfe63b584bfd43
Sayantan <sayantan.dps@gmail.com>
2023-01-25 22:52:53 +0530

    Mobile Responsive

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -4,10 +4,7 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-slate-900 w-screen md:flex md:items-center md:justify-between flex flex-col justify-center items-center md:p-6 shadow-lg text-white">
       <span className="text-sm md:text-md lg:text-lg sm:text-center ">
-        © 2023{" "}
-        <a href="https://flowbite.com/" className="hover:underline">
-          Sayantan Mondal
-        </a>
+        © 2023 <div className="hover:underline">Sayantan Mondal</div>
       </span>
       <div className="flex flex-wrap items-center mt-3 text-sm md:text-md lg:text-lg px-10 sm:mt-0">
         Made with ❤️ Using Gatsby
