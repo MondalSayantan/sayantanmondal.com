3cecffba0b5dcf7aab2f0508820fa3e6a6c08aec
Sayantan <sayantan.dps@gmail.com>
2023-01-25 15:50:08 +0530

    Commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,21 @@
 import "./src/styles/global.css";
 import React from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
-import { SnackbarProvider } from "notistack";
+// import { SnackbarProvider } from "notistack";
 
 export const wrapRootElement = ({ element }) => {
   return (
     <GoogleReCaptchaProvider reCaptchaKey="6Lcb0SEkAAAAANUHfPu4d3_IPdXUKRtf1lW5lo7O">
-      <SnackbarProvider
+      {/* <SnackbarProvider
         maxSnack={1}
         anchorOrigin={{
           vertical: "bottom",
           horizontal: "left",
         }}
         preventDuplicate
-      >
-        {element}
-      </SnackbarProvider>
+      > */}
+      {element}
+      {/* </SnackbarProvider> */}
     </GoogleReCaptchaProvider>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,18 +3,28 @@ import "./Layout.scss";
 import Navbar from "../Navbar";
 import { Header, Skills, Timeline, Projects, Contact } from "../../containers";
 import Carousel from "../Carousel";
+import { SnackbarProvider } from "notistack";
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Navbar />
-      <Header />
-      <Skills />
-      <Projects />
-      <Timeline />
-      <Contact />
-      {/* <Carousel /> */}
-    </div>
+    <SnackbarProvider
+      maxSnack={1}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      preventDuplicate
+    >
+      <div>
+        <Navbar />
+        <Header />
+        <Skills />
+        <Projects />
+        <Timeline />
+        <Contact />
+        {/* <Carousel /> */}
+      </div>
+    </SnackbarProvider>
   );
 };
 
